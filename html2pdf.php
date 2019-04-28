0865cfa4460042cb7b216655767449d5805d0ef1
Timo Lohmann <lohmanntimo@gmail.com>
2019-04-28 15:42:30 +0200

    html2pdftest

--- html2pdf.php
+++ html2pdf.php
@@ -1,4 +1,4 @@
-<php
+
 require __DIR__.'/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
@@ -6,6 +6,3 @@ use Spipu\Html2Pdf\Html2Pdf;
 $html2pdf = new Html2Pdf();
 $html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
 $html2pdf->output();
-
-
-?>
