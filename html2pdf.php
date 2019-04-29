b8124cc7bf48a601a60858d4d63aa470e82a8b57
tiloman <lohmanntimo@gmail.com>
2019-04-29 15:07:09 +0200

    fsdf

--- html2pdf.php
+++ html2pdf.php
@@ -6,13 +6,13 @@ use Spipu\Html2Pdf\Html2Pdf;
 $html2pdf = new HTML2PDF();
 
 $selectedWord = $_SERVER['QUERY_STRING'];
-// $metacom = undefined ;
+$metacom = undefined ;
 
-// if(file_exists("files/metacom/".$selectedWord.".png")) {
-//   $metacom = "<img src='files/metacom/".$selectedWord.".png 'style='height:30%; align: center'>";
-// } else {
-//   $metacom = "Kein Metacom Symbol vorhanden.";
-// }
+if(file_exists("files/metacom/".$selectedWord.".png")) {
+  $metacom = "<img src='files/metacom/".$selectedWord.".png 'style='height:30%; align: center'>";
+} else {
+  $metacom = "Kein Metacom Symbol vorhanden.";
+}
 
 
 $html2pdf->WriteHTML("<html>
