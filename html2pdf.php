208bcf654c2584dbe9f322bc4b5b035e4bdce6cc
tiloman <lohmanntimo@gmail.com>
2019-04-29 16:48:43 +0200

    updated index. added video

--- html2pdf.php
+++ html2pdf.php
@@ -3,31 +3,27 @@ require __DIR__.'/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 
-$html2pdf = new HTML2PDF();
+$html2pdf = new HTML2PDF('L', 'A4', 'de');
 
 $selectedWord = $_SERVER['QUERY_STRING'];
 
 if(file_exists("files/metacom/".$selectedWord.".png")) {
-  $metacom = "<img src='files/metacom/".$selectedWord.".png 'style='height:30%; align: center'>";
+  $metacom = "<img src='files/metacom/".$selectedWord.".png 'style='max-height:80%; width:40%; align: right; float: right'>";
 } else {
   $metacom = "Kein Metacom Symbol vorhanden.";
 }
 
-
-$html2pdf->WriteHTML("<html>
-
-<body>
+$html2pdf->WriteHTML("<page>
 <h1>".$selectedWord."</h1>
-<img src='https://tiloman.mooo.com/gebaerden/files/".$selectedWord.".png 'style='height:30%; align: center'>
+<img src='https://tiloman.mooo.com/gebaerden/files/".$selectedWord.".png 'style='max-height:80%; width:40%; align: left; float: left '>
 
-<br><br>
+
 ".$metacom."
 
 
-</body>
-</html>");
+</page>");
 
 
-$html2pdf->Output('test.pdf');
+$html2pdf->Output($selectedWord.'.pdf');
 
 ?>
