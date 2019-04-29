70ca6ee160d8f020f83031636bfe8c53a0aa324e
tiloman <lohmanntimo@gmail.com>
2019-04-29 15:04:01 +0200

    as

--- html2pdf.php
+++ html2pdf.php
@@ -5,7 +5,7 @@ use Spipu\Html2Pdf\Html2Pdf;
 
 $html2pdf = new HTML2PDF();
 
-// $selectedWord = $_SERVER['QUERY_STRING'];
+$selectedWord = $_SERVER['QUERY_STRING'];
 // $metacom = undefined ;
 
 // if(file_exists("files/metacom/".$selectedWord.".png")) {
@@ -18,7 +18,7 @@ $html2pdf = new HTML2PDF();
 $html2pdf->WriteHTML("<html>
 
 <body>
-<h1>Test</h1>
+<h1>".$selectedWord."</h1>
 <img src='https://tiloman.mooo.com/gebaerden/files/aber.png 'style='height:30%; align: center'>
 
 <br><br>
