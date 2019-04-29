4b549574bea7c3a7f138a2dd954c8cc74863557e
tiloman <lohmanntimo@gmail.com>
2019-04-29 14:31:08 +0200

    html2pdf

--- html2pdf_test.php
+++ html2pdf_test.php
@@ -2,9 +2,22 @@
 require __DIR__.'/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
+$selectedWord = "aber";
+
 
 $html2pdf = new Html2Pdf();
-$html2pdf->writeHTML('<h1>HelloWorld</h1>This is my first test');
+$html2pdf->writeHTML("<html>
+
+<body>
+<h1>".$selectedWord."</h1>
+<p>Die Konvertierung in PDF funktioniert noch nicht einwandfrei</p>
+<img src='https://tiloman.mooo.com/gebaerden/files/".$selectedWord.".png 'style='height:30%; align: center'>
+
+<br><br>
+".$metacom."
+
+</body>
+</html>");
 $html2pdf->output();
 
 ?>
