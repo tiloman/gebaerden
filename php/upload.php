34597512e2bfdbf1679cf75b67069e305a735753
Timo Lohmann <47814142+tiloman@users.noreply.github.com>
2019-05-15 19:13:08 +0200

    Update upload.php

--- php/upload.php
+++ php/upload.php
@@ -16,7 +16,7 @@ $extension = strtolower(pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION));
 
 
 //Überprüfung der Dateiendung
-$allowed_extensions = array('png', 'jpg', 'jpeg', 'gif');
+$allowed_extensions = array('png', 'jpg', 'jpeg', 'gif', 'heic');
 if(!in_array($extension, $allowed_extensions)) {
  die("Ungültige Dateiendung. Nur png, jpg, jpeg und gif-Dateien sind erlaubt");
  $errorMessage = "Nur der Upload von Bilddateien ist gestattet";
