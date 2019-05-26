393cf59af26e62f67664dc8c0db4988e629200f6
Timo Lohmann <lohmanntimo@gmail.com>
2019-05-26 13:40:02 +0200

    bugfix html mail

--- php/addSchool.php
+++ php/addSchool.php
@@ -35,6 +35,14 @@ if(!isset($_SESSION['userid'])) {
 
     ";
 
+    $header  = "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html; charset=utf-8\r\n";
+
+    $header .= "From: $absender\r\n";
+    $header .= "Reply-To: $antwortan\r\n";
+    // $header .= "Cc: $cc\r\n";  // falls an CC gesendet werden soll
+    $header .= "X-Mailer: PHP ". phpversion();
+
     mail($empfaenger, $betreff, $text, $from);
   }
 
