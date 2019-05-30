7760dc03edcb1808c5200b4573b111356023ee78
Timo Lohmann <lohmanntimo@gmail.com>
2019-05-30 17:16:19 +0200

    better quality

--- php/uploadVideoID.php
+++ php/uploadVideoID.php
@@ -78,7 +78,7 @@ if(!$video_error) {
 
 
   //Video Thumbnail erstellen mit ffmpeg
-  echo exec("/volume1/@appstore/ffmpeg/bin/ffmpeg -i $new_path_video -qscale:v 4 -ss 00:00:00.010 -vframes 1 -vf scale=500:-1 $video_upload_folder$word-thumb.jpg >/dev/null 2>/dev/null &");
+  echo exec("/volume1/@appstore/ffmpeg/bin/ffmpeg -i $new_path_video -qscale:v 2 -ss 00:00:00.010 -vframes 1 -vf scale=800:-1 $video_upload_folder$word-thumb.jpg >/dev/null 2>/dev/null &");
   //konvertiere die Datei mit ffmpeg ---- ausgeschaltet wegen langsamer performance...
   //echo exec("/volume1/@appstore/ffmpeg/bin/ffmpeg -i $new_path_video $video_upload_folder$word-converted.mp4 >/dev/null 2>/dev/null &");
 
